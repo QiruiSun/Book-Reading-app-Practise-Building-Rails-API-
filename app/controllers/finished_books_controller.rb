b040afa539186d9e7bed6bc22b3e83c931317764
Qirui Sun <qiruisun@gmail.com>
2016-04-19 17:30:07 -0400

    refactor the query in controller to using scope in the model

--- app/controllers/finished_books_controller.rb
+++ app/controllers/finished_books_controller.rb
@@ -1,6 +1,6 @@
 class FinishedBooksController < ApplicationController
     def index
-      @books = Book.where.not(finished_at: nil)
+      @books = Book.finished
       respond_to do |format|
         format.xml { render xml: @books, status: 200}
         format.json { render json: @books, status: 200 }
