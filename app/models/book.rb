bcccd588d738250f6b993f245d5343b7943eedbb
Qirui Sun <qiruisun@gmail.com>
2016-04-21 16:25:49 -0400

    Add genre and test for genres

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
   scope :finished, -> { where.not(finished_at: nil) }
   validates :title, presence: true
+  belongs_to :genre
 end
--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -2,14 +2,18 @@ require 'test_helper'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
     setup do
-      Book.create!(title: 'Harry Potter', rating: 5)
-      Book.create!(title: 'Enders Game', rating: 4)
+      @genre = Genre.create!(name: "Science Fiction")
+      @genre.books.create!(title: 'Harry Potter', rating: 5)
+      @genre.books.create!(title: 'Enders Game', rating: 4)
     end
     test "listing books" do
       get "/books"
       assert_equal 200, response.status
       assert_equal Mime::JSON, response.content_type
-      assert_equal Book.count, json(response.body)[:books].size
+      books = json(response.body)[:books]
+      assert_equal Book.count, books.size
+      book = Book.find(books.first[:id])
+      assert_equal @genre.id, book.genre.id
     end
 
     test "lists top rated books" do
